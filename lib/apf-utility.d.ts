295ae6f017336c678e39d0abda303bd154b78041
Angr4m <mits.soft.main@gmail.com>
2020-09-08 22:03:10 +0200

    Updated .d.ts

---
+++ lib/apf-utility.d.ts
@@ -0,0 +1,2 @@
+export function run(arg: any, subset: any): any;
+export function import_apf(lang: any): any;
--- lib/dist/apf-utility.d.ts
+++
@@ -1,2 +0,0 @@
-export function run(arg: any, subset: any): Promise<any>;
-export function import_apf(lang: any): Promise<any>;
